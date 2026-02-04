0d5bd6a3a4ba8826021862ef0b20435176d152e8
Leandro Jr. Marques Dos Santos <leandrojr7367@gmail.com>
2026-02-04 10:13:02 -0300

    Todas imagens dos clientes reais adicionadas

--- js/main.js
+++ js/main.js
@@ -1,33 +1,36 @@
-const clients = [
-    { name: "Google", logo: "https://upload.wikimedia.org/wikipedia/commons/2/2f/Google_2015_logo.svg" },
-    { name: "Amazon", logo: "https://upload.wikimedia.org/wikipedia/commons/a/a9/Amazon_logo.svg" },
-    { name: "IBM", logo: "https://upload.wikimedia.org/wikipedia/commons/5/51/IBM_logo.svg" },
-    { name: "Samsung", logo: "https://upload.wikimedia.org/wikipedia/commons/2/24/Samsung_Logo.svg" },
-    { name: "Apple", logo: "https://upload.wikimedia.org/wikipedia/commons/f/fa/Apple_logo_black.svg" },
-    { name: "Microsoft", logo: "https://upload.wikimedia.org/wikipedia/commons/4/44/Microsoft_logo.svg" },
-    { name: "Netflix", logo: "https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg" },
-    { name: "Spotify", logo: "https://upload.wikimedia.org/wikipedia/commons/9/96/Spotify_logo_without_text.svg" },
-    { name: "YouTube", logo: "https://upload.wikimedia.org/wikipedia/commons/e/e1/Logo_of_YouTube_%282015-2017%29.svg" },
-    { name: "Facebook", logo: "https://upload.wikimedia.org/wikipedia/commons/b/b8/2021_Facebook_icon.svg" },
-    { name: "Twitter", logo: "https://upload.wikimedia.org/wikipedia/commons/6/6f/Logo_of_Twitter.svg" },
-    { name: "Instagram", logo: "https://upload.wikimedia.org/wikipedia/commons/a/a5/Instagram_icon.png" }
-];
+const clientImages = [
+        "Cagepa_Logo.png", "Neoenergia_Logo.png", "Rio_Ave_Logo.png", "abreu.png",
+        "ade.png", "amway.png", "assolan-logo.png", "auxiliadora.png", 
+        "batatela.png", "bilio.png", "cattan.png", "cdlrecife.png", 
+        "celpe.png", "claro-logo.png", "clinilaser.png", "coelba-logo.png",
+        "compesa.png", "cosern-logo.png", "energisa-logo.png", "fcdlpe.png",
+        "grupojcpm-logo.png", "habitat.png", "imip-logo.png", "infinito-logo.png",
+        "iquine-logo.png", "jucepe.png", "marambaia.png", "mazzarello.png",
+        "mmsagencia-logo.png", "peconstrutora.png", "prefeitura_recife.png",
+        "rocha.png", "sebrae.png", "sindilojas.png", "stgobain.png", "tim-logo.png"
+    ];
 
-const renderClients = () => {
-    const clientsGrid = document.getElementById('clients-grid');
-    if (clientsGrid) {
-        // Limpa o grid antes de renderizar para evitar duplicatas ao recarregar scripts
-        clientsGrid.innerHTML = ''; 
+    const renderClients = () => {
+        const clientsGrid = document.getElementById('clients-grid');
+        if (clientsGrid) {
+            clientsGrid.innerHTML = ''; 
 
-        clients.forEach(client => {
-            const div = document.createElement('div');
-            div.className = 'client-item';
-            div.title = client.name;
-            div.innerHTML = `<img src="${client.logo}" alt="${client.name}" class="client-logo">`;
-            clientsGrid.appendChild(div);
-        });
-    }
-};
+            clientImages.forEach(fileName => {
+                const div = document.createElement('div');
+                div.className = 'client-item';
+                const clientName = fileName.split('.')[0].replace(/_|-/g, ' ');
+                
+                div.title = clientName;
+                div.innerHTML = `
+                    <img src="images/clientes/${fileName}" 
+                         alt="${clientName}" 
+                         class="client-logo">
+                `;
+                clientsGrid.appendChild(div);
+            });
+        }
+    };
+
 
 document.addEventListener('DOMContentLoaded', () => {
     renderClients();
@@ -37,10 +40,10 @@ document.addEventListener('DOMContentLoaded', () => {
         else header.classList.remove('smaller-header');
     });
 
-    // Lógica do Banner
+
     const slides = document.querySelectorAll('.slide');
     let currentSlide = 0;
-    const delay = 8000; // Tempo original
+    const delay = 8000; 
 
     function showSlide(index) {
         slides.forEach(s => s.classList.remove('active'));
@@ -58,7 +61,6 @@ document.addEventListener('DOMContentLoaded', () => {
         showSlide(prev);
     });
 
-    // Troca automática
     setInterval(() => {
         let next = (currentSlide + 1) % slides.length;
         showSlide(next);
